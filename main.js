20e7a279ceff6859247d20048967306cd2382eea
josedev21 <josedev257@gmail.com>
2023-08-15 02:48:08 -0300

    modificando js

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
+let linhas
 $(document).ready(function() {
-
     $('form').on('submit', function(e) {
         e.preventDefault();
 
-        const Tarefa = $('#adiciona-tarefa').val();
-        const adicionaTarefa = $('<li style="display: none"></li>');
+        const Tarefa = $('#adiciona-tarefa').val();   
+        const novaLinha = $('<tr></tr>');
 
-        $(`<td>${Tarefa} </td>`).appendTo(adicionaTarefa);
+        $(`<td>${Tarefa}</td>`).appendTo(novaLinha);
+        $(novaLinha).appendTo('tbody');
 
-        $(adicionaTarefa).appendTo('td');
-
+        $('#adiciona-tarefa').val('');
     })
+        
 })
